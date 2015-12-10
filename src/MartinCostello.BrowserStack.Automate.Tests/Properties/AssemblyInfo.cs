ff30416e36ebe5b9764506a0ba1914ad692ff802
martincostello <martin@martincostello.com>
2015-12-10 19:05:40 +0000

    Add solution
    Add initial solution structure and build automation files.

---
+++ src/MartinCostello.BrowserStack.Automate.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MartinCostello.BrowserStack.Automate.Tests")]
+[assembly: AssemblyDescription("Tests for MartinCostello.BrowserStack.Automate.")]
+[assembly: CLSCompliant(false)]
+[assembly: Guid("dc723df1-5852-45b1-b2bd-71ba85cded1e")]
