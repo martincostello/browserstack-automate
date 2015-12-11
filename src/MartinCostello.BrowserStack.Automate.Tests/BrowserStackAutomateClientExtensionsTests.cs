f0f0f77b6a63317f2a9ba6e9c0284808a5a1eeb0
martincostello <martin@martincostello.com>
2015-12-11 15:01:22 +0000

    Add tests for BrowserStackAutomateClientExtensions

---
+++ src/MartinCostello.BrowserStack.Automate.Tests/BrowserStackAutomateClientExtensionsTests.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.BrowserStack.Automate
+{
+    using System;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    /// <summary>
+    /// A class containing tests for the <see cref="BrowserStackAutomateClientExtensions"/> class.
+    /// </summary>
+    public static class BrowserStackAutomateClientExtensionsTests
+    {
+        [Fact]
+        public static async Task SetSessionCompletedAsync_Throws_If_Client_Is_Null()
+        {
+            // Arrange
+            BrowserStackAutomateClient client = null;
+
+            string sessionId = "MySessionId";
+            string reason = "My reason";
+
+            // Act and Assert
+            await Assert.ThrowsAsync<ArgumentNullException>("client", () => client.SetSessionCompletedAsync(sessionId, reason));
+        }
+
+        [Fact]
+        public static async Task SetSessionErrorAsync_Throws_If_Client_Is_Null()
+        {
+            // Arrange
+            BrowserStackAutomateClient client = null;
+
+            string sessionId = "MySessionId";
+            string reason = "My reason";
+
+            // Act and Assert
+            await Assert.ThrowsAsync<ArgumentNullException>("client", () => client.SetSessionErrorAsync(sessionId, reason));
+        }
+    }
+}
