1c9200c0ec381d8dc6f656bd24570e04d2e6aa58
martincostello <martin@martincostello.com>
2015-12-11 15:01:32 +0000

    Add copyright header

--- src/MartinCostello.BrowserStack.Automate.Tests/BrowserStackAutomateClientTests.cs
+++ src/MartinCostello.BrowserStack.Automate.Tests/BrowserStackAutomateClientTests.cs
@@ -1,4 +1,7 @@
-﻿namespace MartinCostello.BrowserStack.Automate
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.BrowserStack.Automate
 {
     using System;
     using System.Collections.Generic;
