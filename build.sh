651e73a838184af3960b3955b85e9f3820a8354c
martincostello <martin@martincostello.com>
2016-10-17 18:00:20 +0100

    Only run tests for netcoreapp1.0 in Travis Only run tests for netcoreapp1.0 in Travis and not for .NET Framework.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 dotnet restore --verbosity minimal
 dotnet build src/MartinCostello.BrowserStack.Automate
-dotnet test tests/MartinCostello.BrowserStack.Automate.Tests
+dotnet test tests/MartinCostello.BrowserStack.Automate.Tests --framework "netcoreapp1.0"
 dotnet pack src/MartinCostello.BrowserStack.Automate
