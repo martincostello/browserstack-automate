a16a9d2f397a26642f49e723b839f04ded8f6ab7
Martin Costello <martin@martincostello.com>
2016-11-06 10:53:15 +0000

    Fix build.sh
    Update the TFM for the library to .NET Standard 1.3.
    Do not create the NuGet package.

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 dotnet restore --verbosity minimal
-dotnet build src/MartinCostello.BrowserStack.Automate --framework "netcoreapp1.0"
+dotnet build src/MartinCostello.BrowserStack.Automate --framework "netstandard1.3"
 dotnet test tests/MartinCostello.BrowserStack.Automate.Tests --framework "netcoreapp1.0"
-dotnet pack src/MartinCostello.BrowserStack.Automate --no-build
