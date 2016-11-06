bbbb3fba08857c87748179f3398ced21361935f0
Martin Costello <martin@martincostello.com>
2016-11-06 10:49:19 +0000

    Only build netcoreapp1.0 on Linux
    Only build for netcoreapp1.0 on Linux so that Mono isn't needed on the Travis CI build image.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 dotnet restore --verbosity minimal
-dotnet build src/MartinCostello.BrowserStack.Automate
+dotnet build src/MartinCostello.BrowserStack.Automate --framework "netcoreapp1.0"
 dotnet test tests/MartinCostello.BrowserStack.Automate.Tests --framework "netcoreapp1.0"
-dotnet pack src/MartinCostello.BrowserStack.Automate
+dotnet pack src/MartinCostello.BrowserStack.Automate --no-build
