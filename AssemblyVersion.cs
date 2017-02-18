0d7b636168e54564e409fa63e69b0feca95fb254
martincostello <martin@martincostello.com>
2017-02-18 09:26:23 +0000

    Update version to 3.0.0.0
    Update the assembly version to 3.0.0.0 to represent a breaking change.

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -3,6 +3,6 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.0.2.0")]
-[assembly: AssemblyFileVersion("2.0.2.0")]
-[assembly: AssemblyInformationalVersion("2.0.2.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyInformationalVersion("3.0.0.0")]
