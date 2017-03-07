1810c4492e337fe7706532f0f923f2f3df5a297b
martincostello <martin@martincostello.com>
2017-03-07 22:44:10 +0000

    Build project instead of solution
    Build the project instead of the solution to try and fix Travis CI
    build.

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ export artifacts=$(dirname "$(readlink -f "$0")")/artifacts
 export configuration=Release
 
 dotnet restore MartinCostello.BrowserStack.Automate.sln --verbosity minimal || exit 1
-dotnet build MartinCostello.BrowserStack.Automate.sln --output $artifacts --configuration $configuration --framework "netstandard1.3" || exit 1
+dotnet build src/MartinCostello.BrowserStack.Automate/MartinCostello.BrowserStack.Automate.csproj --output $artifacts --configuration $configuration --framework "netstandard1.3" || exit 1
 dotnet test tests/MartinCostello.BrowserStack.Automate.Tests/MartinCostello.BrowserStack.Automate.Tests.csproj --output $artifacts --configuration $configuration --framework "netcoreapp1.0" || exit 1
