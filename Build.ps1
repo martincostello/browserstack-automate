20e8a44ca286a7b7f6cd973a37b6d96e418c1b41
martincostello <martin@martincostello.com>
2019-07-29 13:52:51 +0100

    Switch to coverlet
    Use coverlet for code coverage instead of OpenCover.

--- Build.ps1
+++ Build.ps1
@@ -2,8 +2,7 @@ param(
     [Parameter(Mandatory = $false)][string] $Configuration = "Release",
     [Parameter(Mandatory = $false)][string] $VersionSuffix = "",
     [Parameter(Mandatory = $false)][string] $OutputPath = "",
-    [Parameter(Mandatory = $false)][switch] $SkipTests,
-    [Parameter(Mandatory = $false)][switch] $DisableCodeCoverage
+    [Parameter(Mandatory = $false)][switch] $SkipTests
 )
 
 $ErrorActionPreference = "Stop"
@@ -95,71 +94,29 @@ function DotNetPack {
 function DotNetTest {
     param([string]$Project)
 
-    if ($DisableCodeCoverage -eq $true) {
-        if ($null -ne $env:TF_BUILD) {
-            & $dotnet test $Project --output $OutputPath --framework $framework --no-build --logger trx
-        }
-        else {
-            & $dotnet test $Project --output $OutputPath --framework $framework --no-build
-        }
+    $nugetPath = Join-Path $env:USERPROFILE ".nuget\packages"
+    $propsFile = Join-Path $solutionPath "Directory.Build.props"
+    $reportGeneratorVersion = (Select-Xml -Path $propsFile -XPath "//PackageReference[@Include='ReportGenerator']/@Version").Node.'#text'
+    $reportGeneratorPath = Join-Path $nugetPath "ReportGenerator\$reportGeneratorVersion\tools\netcoreapp2.0\ReportGenerator.dll"
+
+    $coverageOutput = Join-Path $OutputPath "coverage.cobertura.xml"
+    $reportOutput = Join-Path $OutputPath "coverage"
+
+    if ($null -ne $env:TF_BUILD) {
+        & $dotnet test $Project --output $OutputPath --logger trx
     }
     else {
+        & $dotnet test $Project --output $OutputPath
+    }
 
-        if ($installDotNetSdk -eq $true) {
-            $dotnetPath = $dotnet
-        }
-        else {
-            $dotnetPath = (Get-Command "dotnet.exe").Source
-        }
+    $dotNetTestExitCode = $LASTEXITCODE
 
-        $nugetPath = Join-Path $env:USERPROFILE ".nuget\packages"
-        $propsFile = Join-Path $solutionPath "Directory.Build.props"
-
-        $openCoverVersion = (Select-Xml -Path $propsFile -XPath "//PackageReference[@Include='OpenCover']/@Version").Node.'#text'
-        $openCoverPath = Join-Path $nugetPath "OpenCover\$openCoverVersion\tools\OpenCover.Console.exe"
-
-        $reportGeneratorVersion = (Select-Xml -Path $propsFile -XPath "//PackageReference[@Include='ReportGenerator']/@Version").Node.'#text'
-        $reportGeneratorPath = Join-Path $nugetPath "ReportGenerator\$reportGeneratorVersion\tools\netcoreapp2.0\ReportGenerator.dll"
-
-        $coverageOutput = Join-Path $OutputPath "code-coverage.xml"
-        $reportOutput = Join-Path $OutputPath "coverage"
-
-        if ($null -ne $env:TF_BUILD) {
-            & $openCoverPath `
-                `"-target:$dotnetPath`" `
-                `"-targetargs:test $Project --output $OutputPath --logger trx`" `
-                -output:$coverageOutput `
-                `"-excludebyattribute:System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage*`" `
-                -hideskipped:All `
-                -mergebyhash `
-                -mergeoutput `
-                -oldstyle `
-                -register:user `
-                -skipautoprops `
-                `"-filter:+[MartinCostello.BrowserStack.Automate]* -[MartinCostello.BrowserStack.Automate.Tests]*`"
-        }
-        else {
-            & $openCoverPath `
-                `"-target:$dotnetPath`" `
-                `"-targetargs:test $Project --output $OutputPath`" `
-                -output:$coverageOutput `
-                `"-excludebyattribute:System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage*`" `
-                -hideskipped:All `
-                -mergebyhash `
-                -mergeoutput `
-                -oldstyle `
-                -register:user `
-                -skipautoprops `
-                `"-filter:+[MartinCostello.BrowserStack.Automate]* -[MartinCostello.BrowserStack.Automate.Tests]*`"
-        }
-
-        $dotNetTestExitCode = $LASTEXITCODE
-
+    if (Test-Path $coverageOutput) {
         & $dotnet `
             $reportGeneratorPath `
             `"-reports:$coverageOutput`" `
             `"-targetdir:$reportOutput`" `
-            -reporttypes:HTML`;Cobertura `
+            -reporttypes:HTML `
             -verbosity:Warning
     }
 
